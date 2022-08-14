7e2bb201971874d7e5290f10370bb41cf1d262db
caique <caique_sarkis_corebiz@whirlpool.com>
2022-08-14 20:25:11 -0300

    Responsive shelf module

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,7 +1,34 @@
 import './style.global.css'
+
+import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export default function Banner(){
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidestoshow: 1,
+        slidestoscroll: 1
+    };
     return(
         <section id="banner-section">
+            <Slider {...settings}>
+                <BannerImage/> 
+                <BannerImage/> 
+                <BannerImage/> 
+                <BannerImage/> 
+            </Slider>
+       </section>
+    )
+}
+
+const BannerImage = () => {
+    return (
+        <div>
             <div id="banner-text-container">
                 <p id="banner-small-text">Olá, o que você está <br/> buscando?</p>
                 <p id="banner-big-text">Criar ou migrar seu <br/> e-commerce?</p>
@@ -10,6 +37,6 @@ export default function Banner(){
             <img id="banner-vector" src='/Assets/Banner/banner.png'  alt='Vector Banner'/>
 
             <img id="banner-image" src='/Assets/Banner/img.png'/>
-        </section>
+        </div>
     )
 }
--- src/Components/Shelf/index.js
+++ src/Components/Shelf/index.js
@@ -2,6 +2,35 @@ import './style.global.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import React from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: 'url("/Assets/Prateleira/left-arrow.png")' }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: 'url("/Assets/Prateleira/right-arrow.png")' }}
+      onClick={onClick}
+    />
+  );
+}
+
 export default function Shelf(props){
     const [products , setProducts] = useState([]);
     useEffect(() =>{
@@ -13,57 +42,86 @@ export default function Shelf(props){
         console.log(data)
     })
     },[])
+
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        nextArrow: <SampleNextArrow/>,
+        prevArrow: <SamplePrevArrow/>,
+        responsive: [
+            {
+            breakpoint: 1280,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+            },
+            {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                nextArrow: <></>,
+                prevArrow: <></>
+            }
+            }
+        ]
+    };
    
-    return(
+    return (products.length != 0) && (
         <section id="shelf-section">
              {/* Shelf Title */}
              <h2 id="shelf-title">
                 Mais Vendidos
             </h2>
             <hr className="shelf-divider"/>
-            <div id="shelf-wrapper">
-                <img id="shelf-right-arrow" src='/Assets/Prateleira/left-arrow.png'  alt="Left Arrow"/>
-                    {/* Shelf Products */}
-                    <div id="shelf-products">
-                        {/* Loops through products populating cards. The only exception is the last product that
-                            does not appear on the Figma Layout */}
-                        { products.map(product => {
-                            const installments = product['installments']
-                            let quantity;
+            <Slider {...settings}>
+                { products.map(product => {
+                    const installments = product['installments']
+                    let quantity;
 
-                           {/* Verify if the product can be split into smaller installments */}
-                            if (installments.length >0){
-                                quantity = installments[0].quantity;
-                            }
-                            if (product.productId !== 5){
-                                return(
-                                    <div id="shelf-product">
-                                        <img id="shelf-product-image" src= {product.imageUrl} alt={`Product ${product.productId} Image`}/>
-                                        <div id="shelf-product-info">
-                                            <div id="shelf-product-header">
-                                                <p id="shelf-product-name">{product.productName}</p>
+                    if (installments.length >0){
+                        quantity = installments[0].quantity;
+                    }
+                    if (product.productId !== 5){
+                        return(
+                            <div>
+                                <div id="shelf-product-card">
+                                    <img id="shelf-product-image" src= {product.imageUrl} alt={`Product ${product.productId} Image`}/>
+                                    <div id="shelf-product-info">
+                                        <p id="shelf-product-name">{product.productName}</p>
 
-                                                <img id="shelf-product-rating" src= {`/Assets/Prateleira/stars(${product.stars}).svg`} alt={`${product.stars} stars product`} />
+                                        <img id="shelf-product-rating" src= {`/Assets/Prateleira/stars(${product.stars}).svg`} alt={`${product.stars} stars product`} />
 
-                                                <p id="shelf-product-list-price">
-                                                    {product.listPrice != null ? `de R$ ${product.listPrice/100}` : "  "}
-                                                </p>
-                                            </div>
-                                           
-                                            <p id="shelf-product-price"> por R$ {(product.price/100).toFixed(2)}</p>
-                                            <p id="shelf-product-installments">
-                                            {installments.length > 0 ? `ou em ${quantity} x de R$ ${((product.price/100)/quantity).toFixed(2)}` : " "}
-                                            </p>
-                                            <button id="shelf-product-buy-button" onClick={props.addItem}>Comprar</button>
-                                        </div>
+                                        <p id="shelf-product-list-price">
+                                            {product.listPrice != null ? `de R$ ${product.listPrice/100}` : "  "}
+                                        </p>
+
+                                        <p id="shelf-product-price"> por R$ {(product.price/100).toFixed(2)}</p>
+                                        <p id="shelf-product-installments">
+                                        {installments.length > 0 ? `ou em ${quantity} x de R$ ${((product.price/100)/quantity).toFixed(2)}` : " "}
+                                        </p>
+                                        <button id="shelf-product-buy-button" onClick={props.addItem}>Comprar</button>
                                     </div>
-                                )
-                            }
+                                </div>
+                            </div>
+                        )
+                    }
 
-                        })}
-                    </div>
-                <img id="shelf-left-arrow" src='/Assets/Prateleira/right-arrow.png'  alt="Right Arrow"/>
-            </div>
+                })}
+            </Slider>
+
         </section>
         
     )
